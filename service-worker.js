0a3927ff371ae53e5ead816920c0f0e8cfc01c30
Jimmy Spencer <42553884+thejimmyspencer@users.noreply.github.com>
2026-07-22 15:33:08 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'pulse-v4';
-const ASSETS = ['./', './index.html', './styles.css?v=4', './app.js?v=4', './manifest.webmanifest?v=4', './icons/icon.svg'];
+const ASSETS = ['./', './index.html', './styles.css?v=4', './app-v4.js', './app.js?v=4', './manifest.webmanifest?v=4', './icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
